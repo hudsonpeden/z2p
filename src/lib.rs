71405c15b7b6bc78b79d27705e56643d1f334616
Hudson Peden <hudson@peden.io>
2024-11-26 09:02:50 -0600

    subscriptions endpoint and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,31 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-
-async fn health_check() -> impl Responder {
-    println!("Health check");
-    HttpResponse::Ok()
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
 }
+
+
+async fn health_check() -> HttpResponse {
+    println!("Health check");
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    println!("Subscribe");
+    HttpResponse::Ok().finish()
+}
+
+
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
    let server = HttpServer::new(|| {
        App::new()
            .route("/health_check", web::get().to(health_check))
+           .route("/subscriptions", web::post().to(subscribe))
    })
    .listen(listener)?
    .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -31,3 +31,52 @@ fn spawn_app() -> String {
     
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    
+    // act
+    let body = "name=mario%20luigi&email=mario%40example.com";
+    let response = client
+    .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+    .expect("Failed to execute request.");
+    
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=mario", "missing the email"),
+        ("email=mario@example.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+    
+    for (invalid_body, error_message) in test_cases {
+        // assert
+        let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(invalid_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+        
+        // act
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 bad request when the payload was {}",
+            error_message
+        )
+    }
+}

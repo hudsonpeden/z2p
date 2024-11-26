0a782125c4b2da85794f978cd5851852de877f60
Hudson Peden <hudson@peden.io>
2024-11-26 10:08:28 -0600

    refactoring structure into modules

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -25,7 +25,7 @@ fn spawn_app() -> String {
     
     let port = listener.local_addr().unwrap().port(); 
     
-    let server = zero2prod::run(listener).expect("Failed to run server");
+    let server = zero2prod::startup::run(listener).expect("Failed to run server");
     
     let _ = tokio::spawn(server);
     
